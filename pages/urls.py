e34a03666a7602069c9f7ff5c320e8415e94b3e4
gurcan <gurcan@veriteknik.com>
2020-05-02 10:18:10 +0300

    db update

--- pages/urls.py
+++ pages/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('genelindirme/', views.genelindirme, name='genelindirme'),
+    path('update_status/', views.update_status, name='update_status'),
     path('export/', views.export, name='export'),
-    path('gmpMesajlari/', views.gmpMesajlari, name='gmpMesajlari'),
-    path('infoTable/', views.infoTable, name='infoTable'),
+    path('gmp_messages/', views.gmp_messages, name='gmp_messages'),
+    path('info_table/', views.info_table, name='info_table'),
     path('db_test/', views.db_test, name='db_test'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from . models import UpdateChunkRequests
 
+
 def index(request):
     return render(request, 'index.html')
 
@@ -11,20 +12,20 @@ def navbar(request):
     return render(request, 'navbar.html')
 
 
-def genelindirme(request):
-    return render(request, 'genelindirme.html')
+def update_status(request):
+    return render(request, 'update_status.html')
 
 
 def export(request):
     return render(request, 'export.html')
 
 
-def gmpMesajlari(request):
-    return render(request, 'gmpMesajlari.html')
+def gmp_messages(request):
+    return render(request, 'gmp_messages.html')
 
 
-def infoTable(request):
-    return render(request, 'infoTable.html')
+def info_table(request):
+    return render(request, 'info_table.html')
 
 
 def db_test(request):
